21cf5636bb37fd572380056795cb192f6ffbeca2
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-10-28 16:20:42 +0900

    var 문법 체험 (변경 가능 변수)

--- app/src/main/java/kr/ac/wku/kotlinbasic_20231028/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/kotlinbasic_20231028/MainActivity.kt
@@ -3,6 +3,8 @@ package kr.ac.wku.kotlinbasic_20231028
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.ac.wku.kotlinbasic_20231028.databinding.ActivityMainBinding
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
+
         binding.btnLog.setOnClickListener{
 //            로그 버튼이 클릭되면 실행될 코드 스코프
 
@@ -24,6 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnToast.setOnClickListener{
             Toast.makeText(this, "토스트 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnVariable.setOnClickListener {
+//            코틀린 변수 문법 연습
+//            내 이름 저장할 변수 (String) 생성 => 변동 가능성 있다
+
+            var myName : String
+
+            myName = "우승진"
+
+//            이름 변경 => 진승우 (역순) => var로 변수를 선언했기 때문에 가능.
+
+            myName = "진승우"
+
+            Toast.makeText(this, myName, Toast.LENGTH_SHORT).show()
         }
     }
 }
